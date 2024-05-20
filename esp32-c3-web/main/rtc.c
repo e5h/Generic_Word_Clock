0a5b8ed47232b5b3b8447d7f5daf10ffb620fc09
abright <ajbright22@gmail.com>
2024-05-20 17:01:15 -0600

    Enhancement #2,"Implement real time clock"
    Added i2c manager functions to the rtc module.

--- esp32-c3-web/main/rtc.c
+++ esp32-c3-web/main/rtc.c
@@ -26,6 +26,7 @@
 /*=============================================================================*/
 
 #include "rtc.h"
+#include "i2c_manager.h"
 
 /*=============================================================================*/
 /*][ LOCAL : Constants and Types ][============================================*/
@@ -50,17 +51,29 @@ static RTC_T rtc_s;
 
 /**===< local >================================================================
  * NAME:
+ *      i2c_write() - write to one or more sequential registers on the RTC
  *
  * SUMMARY:
+ *      using the i2c_manager component, this function will write to one
+ *      or more sequential registers on the PCF85263A real time clock.
  *
  * INPUT REQUIREMENTS:
+ *      - buffer must not be null
  *
  * OUTPUT GUARANTEES:
+ *      - operation will not trigger a race condition
+ *      - status of the operation will be returned
  **===< local >================================================================*/
-static STATUS_E i2c_write(UINT8 dev_addr_u8, UINT8 reg_addr_u8, UINT8* buf_u8, INT32 len_i32)
+static STATUS_E i2c_write(UINT8 reg_addr_u8, UINT8* buf_u8, UINT16 len_u16)
 {
     STATUS_E status_e = STATUS_OK;
-    // TODO: Managed i2c write
+
+    esp_err_t err = i2c_manager_write(I2C_NUM_0, PCF85263A_ADDR_7BIT, reg_addr_u8, buf_u8, len_u16);
+
+    if(err != ESP_OK)
+    {
+        status_e = STATUS_ERR;
+    }
 
     return status_e;
 }
@@ -68,17 +81,29 @@ static STATUS_E i2c_write(UINT8 dev_addr_u8, UINT8 reg_addr_u8, UINT8* buf_u8, I
 
 /**===< local >================================================================
  * NAME:
+ *      i2c_write() - read from one or more sequential registers on the RTC
  *
  * SUMMARY:
+ *      using the i2c_manager component, this function will read from one
+ *      or more sequential registers on the PCF85263A real time clock.
  *
  * INPUT REQUIREMENTS:
+ *      - buffer must not be null
  *
  * OUTPUT GUARANTEES:
+ *      - operation will not trigger a race condition
+ *      - status of the operation will be returned
  **===< local >================================================================*/
-static STATUS_E i2c_read(UINT8 dev_addr_u8, UINT8 reg_addr_u8, UINT8* buf_u8, INT32 len_i32)
+static STATUS_E i2c_read(UINT8 reg_addr_u8, UINT8* buf_u8, INT16 len_u16)
 {
     STATUS_E status_e = STATUS_OK;
-    // TODO: Managed i2c read
+
+    esp_err_t err = i2c_manager_read(I2C_NUM_0, PCF85263A_ADDR_7BIT, reg_addr_u8, buf_u8, len_u16);
+
+    if(err != ESP_OK)
+    {
+        status_e = STATUS_ERR;
+    }
 
     return status_e;
 }
